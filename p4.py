f23c292dd4da38cb810ae0d156f5365abe3bd1cb
unknown <lareola92@gmail.com>
2015-07-05 23:17:14 +0300

    wrong text

--- p4.py
+++ p4.py
@@ -1,17 +1,14 @@
 
 def tgb():
     for x in ['ten', 'nine', 'eight', 'seven', 'six', 'five', 'four', 'tree', 'two', 'one']:
-        print x
-        print " green bottles hanging on the wall,"
-        print x
-        print " green bottles hanging on the wall,"
+        print x, " green bottles hanging on the wall,"
+        print x, " green bottles hanging on the wall,"
         print "And if one green bottle should accidentally fall"
         if x == 'one':
-            print "There’ll be no green bottles hanging on the wall."
+            print "There'll be no green bottles hanging on the wall."
             break
-        print "There’ll be ", x, "green bottles hanging on the wall."
+        print "There'll be ", x, "green bottles hanging on the wall."
 
 
-
-
+tgb()
 
